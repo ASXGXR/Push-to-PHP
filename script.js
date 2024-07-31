a7ea6b8002840459f6da5767b3eb23c6c27e9174
ASXGXR <86375300+ASXGXR@users.noreply.github.com>
2024-07-31 12:41:41 +0000

    Commit

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ document.getElementById('enquiryForm').addEventListener('submit', function(event
 
     const formData = new FormData(this);
 
-    fetch('https://cors-anywhere.herokuapp.com/https://3507.co.uk/enquiries/enquiry_in.php', {
+    fetch('https://thingproxy.freeboard.io/fetch/https://3507.co.uk/enquiries/enquiry_in.php', {
         method: 'POST',
         body: formData
     })
-        .then(response => response.text())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
         .then(data => {
             alert('Form submitted successfully!');
         })
